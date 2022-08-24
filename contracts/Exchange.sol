4a04ad41757b60b885a87e5536e184de8b1fd6d4
Submarinatx <submarinati@gmail.com>
2022-08-24 13:46:09 -0500

    10. Withdraw tokens

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -10,6 +10,7 @@ contract Exchange {
 	mapping(address => mapping(address => uint256)) public tokens;
 
 	event Deposit(address token, address user, uint256 amount, uint256 balance);
+	event Withdraw(address token, address user, uint256 amount, uint256 balance);
 
 	constructor(address _feeAccount, uint256 _feePercent) {
 		feeAccount = _feeAccount;
@@ -22,6 +23,16 @@ contract Exchange {
 		tokens[_token][msg.sender] = tokens[_token][msg.sender] + _amount;
 
 		emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]);
+	}
+
+	function withdrawToken(address _token, uint256 _amount) public {
+
+		Token(_token).transfer(msg.sender, _amount);
+
+		tokens[_token][msg.sender] = tokens[_token][msg.sender] - _amount;
+
+		emit Withdraw(_token, msg.sender, _amount, tokens[_token][msg.sender]);
+
 	}
 
 	function balanceOf(address _token, address _user) public view returns (uint256) {
