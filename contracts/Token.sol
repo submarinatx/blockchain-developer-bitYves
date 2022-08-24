8b316c1c2d863fb8a94cf8f8a8b38f9ff37b40ce
root <root@LENOVO.localdomain>
2022-08-24 06:06:08 -0500

    5. Token transfers

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -11,7 +11,7 @@ contract Token {
 
 	mapping(address => uint256) public balanceOf;
 
-
+	event Transfer(address indexed from, address indexed to, uint256 value);
 
 	constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
 		name = _name;
@@ -19,5 +19,17 @@ contract Token {
 		totalSupply = _totalSupply * (10**decimals);
 		balanceOf[msg.sender] = totalSupply;
 	}
-} 
 
+	function transfer(address _to, uint256 _value) public returns (bool success) {
+
+		require(balanceOf[msg.sender] >= _value);
+		require(_to != address(0));
+
+		balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
+		balanceOf[_to] = balanceOf[_to] + _value;
+
+		emit Transfer(msg.sender, _to, _value);
+
+		return true;
+	}
+} 
