2faac2947bc15fb835cb87f52230bef6194f6464
root <root@LENOVO.localdomain>
2022-08-24 07:19:10 -0500

    6. Token approvals

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -10,18 +10,35 @@ contract Token {
 	uint256 public totalSupply;
 
 	mapping(address => uint256) public balanceOf;
+	mapping(address => mapping(address => uint256)) public allowance;
 
-	event Transfer(address indexed from, address indexed to, uint256 value);
+	event Transfer(
+		address indexed from, 
+		address indexed to, 
+		uint256 value
+	);
 
-	constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
+	event Approval(
+		address indexed owner, 
+		address indexed spender, 
+		uint256 value
+	);
+
+	constructor(
+		string memory _name, 
+		string memory _symbol, 
+		uint256 _totalSupply
+	) {
 		name = _name;
 		symbol = _symbol;
 		totalSupply = _totalSupply * (10**decimals);
 		balanceOf[msg.sender] = totalSupply;
 	}
 
-	function transfer(address _to, uint256 _value) public returns (bool success) {
-
+	function transfer(address _to, uint256 _value) 
+	    public 
+	    returns (bool success) 
+	{
 		require(balanceOf[msg.sender] >= _value);
 		require(_to != address(0));
 
@@ -30,6 +47,17 @@ contract Token {
 
 		emit Transfer(msg.sender, _to, _value);
 
+		return true;
+	}
+
+	function approve(address _spender, uint256 _value) 
+		public 
+		returns(bool success)
+	{
+		require(_spender != address(0));
+		allowance[msg.sender][_spender] = _value;
+
+		emit Approval(msg.sender, _spender, _value);
 		return true;
 	}
 } 
